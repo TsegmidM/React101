e7d94c67b8bdda7fc8682372e816ca99808d5315
TsegmidM <tsegmid.education.icodice.com>
2022-10-31 23:43:31 -0500

    tictactoeupdate

--- src/components/tictactoe/tictactoe.js
+++ src/components/tictactoe/tictactoe.js
@@ -1,25 +1,52 @@
-import React, {useState} from "react";
-import TicTac from "./tictac";
+import React, { useState } from "react";
 import './tictactoe.css'
-const TicTacToe = () => {
-    const squares = [1,2,3,4,5,6,7,8,9]
-    const [xOrO, setXorO] = useState(0);
 
-    return(
-        <div className="container">
-            {squares.map((n,nIdx) => {
-                return xOrO === n ? (
-                <button className="tictactoe-btn"
-                onClick={() => setXorO(()=>n)}
-                >
-                    <TicTac choice="O"/>
-                </button>
-                )
-                :
-                <button className="tictactoe-btn"
-                onClick={() => setXorO(()=>n)}
-                ></button>    
-            })}
+const TicTacToe = () => {
+    let draw = null, status, winner = null;
+    const [boardSquares, setBoardSquares] = useState(Array(9).fill(null))
+    const [isXTrue, setIsXTrue] = useState(true);
+    const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < winningLines.length; i++) {
+        const [a, b, c] = winningLines[i];
+        if (boardSquares[a] && boardSquares[a] === boardSquares[b] && boardSquares[b] === boardSquares[c])
+            winner = boardSquares[c];
+    }
+
+    if (boardSquares.every((square) => square !== null) && !winner) {
+        draw = true;
+    }
+    status = draw ? "Draw!" : winner ? `Winner is: ${winner}` : `${isXTrue ? "X" : "O"}'s turn`
+
+    return (
+        <div className="ttt-container">
+            <div className="status">{status}</div>
+            <div className="board">
+                {boardSquares.map((cell, nIdx) => {
+                    return (
+                        <div className="cell" key={nIdx} onClick={() => {
+                            if (winner || boardSquares[nIdx]) return;
+                            boardSquares[nIdx] = isXTrue ? "X" : "O";
+                            setBoardSquares(boardSquares);
+                            setIsXTrue(!isXTrue);
+                        }
+                        }
+                        > {cell}</div>
+                    )
+                })}
+            </div>
+            <button className="ttt-button" onClick={() => {
+                setBoardSquares(Array(9).fill(null));
+                setIsXTrue('X');
+            }}>Reset</button>
         </div>
     )
 }
