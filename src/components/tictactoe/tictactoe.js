a692d649a7ebdda11082d3acee7058a505548cdf
TsegmidM <Tsegmid@education.icodice.com>
2022-11-10 14:01:32 -0600

    tictactoeupdate

--- src/components/tictactoe/tictactoe.js
+++ src/components/tictactoe/tictactoe.js
@@ -1,53 +1,88 @@
 import React, { useState } from "react";
-import './tictactoe.css'
+import "./tictactoe.css";
 
-const TicTacToe = () => {
-    let draw = null, status, winner = null;
-    const [boardSquares, setBoardSquares] = useState(Array(9).fill(null))
-    const [isXTrue, setIsXTrue] = useState(true);
-    const winningLines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    for (let i = 0; i < winningLines.length; i++) {
-        const [a, b, c] = winningLines[i];
-        if (boardSquares[a] && boardSquares[a] === boardSquares[b] && boardSquares[b] === boardSquares[c])
-            winner = boardSquares[c];
+const winningLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+export default function TicTacToe2() {
+  const [boardData, setBoardData] = useState({});
+  const [currSymbol, setCurrSymbol] = useState("X");
+  const [winner, setWinner] = useState(false);
+  const [draw, setDraw] = useState(false);
+  let status;
+
+  const checkWinner = (cellIdx) => {
+    console.log(boardData, Object.keys(boardData).length);
+    if (Object.keys(boardData).length + 1 >= 5) {
+      let winOptions = winningLines.filter((value) => {
+        if (value.includes(cellIdx)) return true;
+        else return false;
+      });
+
+      winOptions.forEach((winOption) => {
+        const isWin = winOption.every((winLocation) => {
+          if (winLocation === cellIdx) return true;
+
+          return boardData[winLocation] === currSymbol;
+        });
+
+        if (isWin === true) {
+          setWinner(true);
+        } else if (Object.keys(boardData).length === 8 && !winner) {
+          setDraw(true);
+        }
+      });
     }
 
-    if (boardSquares.every((square) => square !== null) && !winner) {
-        draw = true;
-    }
-    status = draw ? "Draw!" : winner ? `Winner is: ${winner}` : `${isXTrue ? "X" : "O"}'s turn`
+    setBoardData((currState) => {
+      return { ...currState, [cellIdx]: currSymbol };
+    });
+    setCurrSymbol((currState) => (currState === "X" ? "O" : "X"));
+  };
 
-    return (
-        <div className="ttt-container">
-            <div className="status">{status}</div>
-            <div className="board">
-                {boardSquares.map((cell, nIdx) => {
-                    return (
-                        <div className="cell" key={nIdx} onClick={() => {
-                            if (winner || boardSquares[nIdx]) return;
-                            boardSquares[nIdx] = isXTrue ? "X" : "O";
-                            setBoardSquares(boardSquares);
-                            setIsXTrue(!isXTrue);
-                        }
-                        }
-                        > {cell}</div>
-                    )
-                })}
+  status = draw
+    ? "Draw!"
+    : winner
+    ? `Winner is: ${currSymbol === "X" ? "O" : "X"}`
+    : `${currSymbol}'s turn`;
+
+  return (
+    <div className="ttt-container">
+      <div className="status">{status}</div>
+      <div className="board">
+        {[...Array(9)].map((cell, cellIdx) => {
+          return (
+            <div
+              className="cell"
+              key={cellIdx}
+              onClick={() => {
+                if (winner || boardData[cellIdx]) return;
+                checkWinner(cellIdx);
+              }}
+            >
+              {boardData[cellIdx]}
             </div>
-            <button className="ttt-button" onClick={() => {
-                setBoardSquares(Array(9).fill(null));
-                setIsXTrue('X');
-            }}>Reset</button>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <button
+        className="ttt-button"
+        onClick={() => {
+          setBoardData({});
+          setWinner(false);
+          setCurrSymbol("X");
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
-export default TicTacToe;
