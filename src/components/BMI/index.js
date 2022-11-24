27065eab03a72ef7031c34b2cdc4187c18399174
TsegmidM <Tsegmid@education.icodice.com>
2022-11-23 20:11:57 -0600

    BMI fix

--- src/components/BMI/index.js
+++ src/components/BMI/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import {
   LineChart,
@@ -8,6 +9,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 import "./bmi-index.css";
 
@@ -18,6 +20,19 @@ const BMITracker = () => {
   });
   const [bmiIndex, setBmiIndex] = useState([]);
   const [byMetric, SetByMetric] = useState(true);
+  const [activeIndex, setActiveIndex] = useState({});
+  
+  let highestBmi =
+    bmiIndex.length > 0
+      ? Math.max(...bmiIndex.map((o) => parseFloat(o.BMI)))
+      : 0;
+
+  console.log(bmiIndex);
+  console.log(highestBmi);
+
+  useEffect(() => {
+    setActiveIndex({ first: bmiIndex.length - 7, second: bmiIndex.length });
+  }, [bmiIndex]);
 
   const onInputChange = (e) => {
     const fieldName = e.target.name;
@@ -26,10 +41,10 @@ const BMITracker = () => {
       ...currState,
       [fieldName]: fieldValue,
     }));
-    console.log("called", bmiData);
   };
 
   const onFormSubmit = (e) => {
+    //  console.log(Math.max(bmiIndex.BMI));
     e.preventDefault();
     let bmiByLb = ((bmiData.weight / bmiData.height ** 2) * 703).toFixed(1);
     let bmiByMetric = (bmiData.weight / (bmiData.height / 100) ** 2).toFixed(1);
@@ -39,31 +54,69 @@ const BMITracker = () => {
         ...currState,
         {
           BMI: byMetric ? bmiByMetric : bmiByLb,
-          date: new Date().toString().slice(0, 10),
+          // date: new Date().toString().slice(0, 10),
+          date: `day:${bmiIndex.length + 1}`,
         },
-      ]
+      ];
     });
     setBmiData({
       height: "",
       weight: "",
     });
-
   };
 
   return (
     <div className="bmi-tracker">
       <div className="bmi-buttons">
-        <button>{"<"}</button>
-        <button>{">"}</button>
+        <button
+          onClick={() => {
+            console.log("ss", activeIndex);
+            setActiveIndex((currState) => {
+              if (currState.first > 0) {
+                return {
+                  currState,
+                  first: currState.first - 1,
+                  second: currState.second - 1,
+                };
+              } else {
+                return currState;
+              }
+            });
+          }}
+        >
+          {"<"}
+        </button>
+        <button
+          onClick={() => {
+            setActiveIndex((currState) => {
+              if (currState.second === bmiIndex.length) {
+                return currState;
+              } else {
+                return {
+                  currState,
+                  first: currState.first + 1,
+                  second: currState.second + 1,
+                };
+              }
+            });
+            console.log("ss", activeIndex);
+          }}
+        >
+          {">"}
+        </button>
       </div>
-      <div className="bmi-form" >
+      <div className="bmi-form">
         <div> BMI TRACKER {byMetric ? "by metric" : "by lbs"}</div>
-        <button onClick={() => {
-          SetByMetric(!byMetric);
-        }}> {byMetric ? "Change to Pound system" : "Change to Metric system"} </button>
+        <button
+          onClick={() => {
+            SetByMetric(!byMetric);
+          }}
+        >
+          {" "}
+          {byMetric ? "Change to Pound system" : "Change to Metric system"}{" "}
+        </button>
 
         <form onSubmit={onFormSubmit}>
-
           <label>
             {byMetric ? "cm:" : "Inch: "}
             <input
@@ -95,33 +148,34 @@ const BMITracker = () => {
           <button
             className="bmi-submit-button"
             type="submit"
-            onClick={() => {
-
-            }}
+            onClick={() => {}}
           >
             Calculate
           </button>
         </form>
       </div>
       <LineChart
-        width={800}
-        height={300}
-        data={bmiIndex.slice(-7)}
+        width={900}
+        height={450}
+        data={
+          bmiIndex.length > 7
+            ? bmiIndex.slice(activeIndex.first, activeIndex.second)
+            : bmiIndex.slice(-7)
+        }
         margin={{
           top: 15,
           right: 30,
           left: 0,
           bottom: 5,
         }}
-
       >
-
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="date"
-        />
+        <XAxis dataKey="date" />
         <YAxis
-          dataKey="BMI" />
+          dataKey="BMI"
+          tickCount={bmiIndex.length < 5 ? 5 : 10}
+          domain={[0, highestBmi]}
+        />
         <Tooltip />
         <Legend />
         <Line
@@ -129,7 +183,6 @@ const BMITracker = () => {
           dataKey="BMI"
           stroke="#8a2be2"
           activeDot={{ r: 8 }}
-
         />
       </LineChart>
     </div>
